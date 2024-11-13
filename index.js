480e78392f4a92fbcb06c7b553053d52102731d5
irdepla <rixsiboyevislombek@gmail.com>
2024-11-13 08:29:34 +0500

    add exercises done

---
+++ index.js
@@ -0,0 +1,105 @@
+// Exercise 1
+
+// function helloName(name){
+//     return `Hello ${name}!`
+//   }
+
+// Exercise 2
+
+// function makeAbba(a, b){
+//     return `${a}${b}${b}${a}`;
+//   }
+
+// Exercise 3
+
+// function makeTags(tag, word){
+//     return `<${tag}>${word}</${tag}>`;
+//   }
+
+// Exercise 5
+
+// function extraEnd(str){
+//     let newStr = str.slice(-2);
+//     return newStr.repeat(3);
+// }
+
+// console.log(extraEnd("Hello"));
+
+// Exercise 6
+
+// function firstTwo(str){
+//     if(str.length < 2){
+//        return str;
+//        }
+//     else{
+//     let newStr = str.slice(0,2)
+//     return newStr;
+//     }
+//   }
+
+// console.log(firstTwo("Hello"));
+// console.log(firstTwo(""));
+
+// Exercise 7
+
+// Shartida xatolik borakan logikasi boyicha tori qilib yechdim.
+
+// function firstHalf(str) {
+//   if (str.length % 2 == 0) {
+//     return str.slice(0, str.length / 2 );
+//   }
+//   else{
+//     return str;
+//   }
+// }
+
+// console.log(firstHalf("Woohoo"));
+// console.log(firstHalf("WooHoo"));
+// console.log(firstHalf("Hello"));
+
+// Exercise 8
+
+// function withoutEnd(str) {
+//   return str.slice(1, -1);
+// }
+
+// console.log(withoutEnd("Hello"));
+
+// Exercise 9
+
+// function comboString(a, b) {
+//   if (a.length < b.length) {
+//     return a + b + a;
+//   } else if (a.length == b.length) {
+//     return a + b;
+//   } else {
+//     return b + a + b;
+//   }
+// }
+
+// console.log(comboString('Hello', 'hi'));
+
+// Exercise 10
+
+// function nonStart(a, b) {
+//   return a.slice(1) + b.slice(1);
+// }
+
+// console.log(nonStart("Hello", "There"));
+
+
+
+// Exercise 11
+
+
+// function left2(str){
+//     let subStr = str.substring(2)
+//     let newStr = str.slice(0,2)
+//     return subStr + newStr
+//   }
+
+
+//   console.log(left2('Hello'));
+
+
+
